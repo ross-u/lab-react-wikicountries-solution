28f023446f15543446d5e2dd2446094dd9b4d5df
ross-u <cirkovic.uros.srb@gmail.com>
2021-12-02 18:34:47 +0000

    Lab solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import CountryDetails from './components/CountryDetails';
 import CountriesList from './components/CountriesList';
 import countriesData from './countries.json';
@@ -8,6 +9,7 @@ import countriesData from './countries.json';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div class="container">
         <div class="row">
           <CountriesList countriesData={countriesData} />
